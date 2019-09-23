7579750af3cb8a138d2a07da24c616e1333b9442
Zarktoss <zarktos@hotmail.com>
2019-09-23 21:45:09 +0200

    Test

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print ('Hello world')
+#print ('Hello world') Directly shows in the screen  what we writed
+
+#name = input('What is your name? ') #Input gets one we write into a variable
+#print('hello')
+#print(name)
+print ('adding numbers')
+x = 532 + 12
+print ('division')
+y = x /2
+print ('doing math bitch')
