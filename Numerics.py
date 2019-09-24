ee09d73c3ae8611c3b2ff71f9a999b4222cabfe4
Zarktoss <zarktos@hotmail.com>
2019-09-24 21:01:07 +0200

    Nuevos Test

---
+++ Numerics.py
@@ -0,0 +1,5 @@
+#days_in_feb = 28
+
+first_num = input ('Enter first number ')
+second_num = input ('Enter second number ') # If we put an input we allways get strings so we have to convert after the numbers.
+print(int(first_num) + int(second_num)) # We put str/int to transform de number to concatenate it with the string or sum numbers
--- Test.py
+++ Test.py
@@ -1,10 +1,16 @@
-#print ('Hello world') Directly shows in the screen  what we writed
+# print ('Hello world') Directly shows in the screen  what we writed
 
-#name = input('What is your name? ') #Input gets one we write into a variable
-#print('hello')
-#print(name)
+# name = input('What is your name? ') #Input gets one we write into a variable
+# print('hello')
+# print(name)
 print ('adding numbers')
 x = 532 + 12
 print ('division')
 y = x /2
 print ('doing math bitch')
+print ('x=',x,'y=',y)
+
+first_name = 'Zarktos'
+last_name = 'XXX'
+print(first_name + last_name) #concatenate with +
+print('Hello '.upper()) # .upper/lower for modificate strings (upper, lower etc.)
