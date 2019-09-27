da7b63c810b86ad4da6bd792a3e66da8fffd2bda
Zarktoss <zarktos@hotmail.com>
2019-09-27 19:44:05 +0200

    saving dates

---
+++ Date.py
@@ -0,0 +1,7 @@
+# To get current date and time
+# we need to use the datetime library
+from datetime import datetime
+
+curent_date = datetime.now()
+# the now function returns a datetime object
+print('Today is: ' + str(current_date))
